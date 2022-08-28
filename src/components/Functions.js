efc7d208af0621631111baae286e71aadba84c67
DariusAndrijauskas <Darius.Andrijauskas@gmail.com>
2022-08-28 15:58:21 +0300

    first commit

---
+++ src/components/Functions.js
@@ -0,0 +1,27 @@
+module.exports = {
+    fetchPost: (url, data, callback) => {
+        const options = {
+            method: 'POST',
+            headers: {"content-type":"application/json"},
+            body: JSON.stringify(data),
+            credentials: 'include'
+        }
+        fetch(`http://localhost:4000/${url}`, options)
+        .then(res => res.json())
+        .then(callback)
+    },
+    fetchGet: (url, callback) => {
+        const options = {
+            method: 'GET',
+            headers: {"content-type":"application/json"},
+            credentials: 'include'
+        }
+        fetch(`http://localhost:4000/${url}`, options)
+        .then(res => res.json())
+        .then(callback)
+    },
+    citiesList: ['Vilnius', 'Kaunas', 'Klaipėda', 'Šiauliai', 'Panevėžys', 'Utena', 'Marijampolė', 'Telšiai', 'Alytus', 'Tauragė'],
+    genderList: ['male', 'female']
+}
+
+// fetchPost('setFilter', filter, data => setUser(data.user))
